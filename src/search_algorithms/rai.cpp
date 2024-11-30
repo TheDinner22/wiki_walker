a5fda78c3241c56676abe28d29592b6a4113a744
rainierstfort <rainierstfort28@gmail.com>
2024-11-30 10:54:33 -0500

    updated with bfs/dfs algos

--- src/search_algorithms/rai.cpp
+++ src/search_algorithms/rai.cpp
@@ -1,4 +1,11 @@
 #include "search_algorithms.hpp"
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <unordered_set>
 
 ParseResults rai_algo(const std::string& start, std::string& end){
     ParseResults r;
@@ -6,6 +13,69 @@ ParseResults rai_algo(const std::string& start, std::string& end){
     r.pages_visited = 10;
     r.algo_name = "rai button";
     r.shortest_path = {"a", "b", "c"};
+
+    bool bfs(const map<string, vector<string>>& graph, const string& startUrl, const string& endUrl) {
+        queue<string> toVisit;
+        unordered_set<string> visited;
+
+        toVisit.push(startUrl);
+        visited.insert(startUrl);
+
+        while (!toVisit.empty()) {
+            string currentUrl = toVisit.front();
+            toVisit.pop();
+
+            if (currentUrl == endUrl) {
+                cout << "Found URL using BFS: " << currentUrl << endl;
+                return true;
+            }
+
+            if (graph.find(currentUrl) != graph.end()) {
+                for (const string& neighbor : graph.at(currentUrl)) {
+                    if (visited.find(neighbor) == visited.end()) {
+                        toVisit.push(neighbor);
+                        visited.insert(neighbor);
+                    }
+                }
+            }
+        }
+        cout << "End URL not found using BFS." << endl;
+        return false;
+    }
+
+    // DFS Function
+    bool dfs(const map<string, vector<string>>& graph, const string& startUrl, const string& endUrl) {
+        stack<string> toVisit;
+        unordered_set<string> visited;
+
+        toVisit.push(startUrl);
+
+        while (!toVisit.empty()) {
+            string currentUrl = toVisit.top();
+            toVisit.pop();
+
+            if (visited.find(currentUrl) != visited.end()) {
+                continue;
+            }
+
+            visited.insert(currentUrl);
+
+            if (currentUrl == endUrl) {
+                cout << "Found URL using DFS: " << currentUrl << endl;
+                return true;
+            }
+
+            if (graph.find(currentUrl) != graph.end()) {
+                for (const string& neighbor : graph.at(currentUrl)) {
+                    if (visited.find(neighbor) == visited.end()) {
+                        toVisit.push(neighbor);
+                    }
+                }
+            }
+        }
+        cout << "End URL not found using DFS." << endl;
+        return false;
+    }
 
     return r;
 }
